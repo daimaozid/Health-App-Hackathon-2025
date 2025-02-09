ad9ab15361ffa6c445ceec8caf5941b1e92117b4
daimaozid <137755814+daimaozid@users.noreply.github.com>
2025-02-08 20:28:50 -0500

    Add files via upload

---
+++ healthapp/src/Controller.java
@@ -0,0 +1,21 @@
+//Imports
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    //Add functions for buttons below
+
+    public void leftButton(ActionEvent e) {
+        System.out.println("Left has been pressed!");
+    }
+    
+    public void rightButton(ActionEvent e) {
+        System.out.println("Right has been pressed!");
+    }
+
+    public void homeButtom(ActionEvent e) {
+        System.out.println("Home has been pressed!");
+    }
+
+}
